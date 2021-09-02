26b0d2561bd2a49f01440366e411d17dff9b0efa
robbie <robbielcampbell@hotmail.com>
2021-09-02 17:46:08 +0100

    :rocket: just a little bit today :rocket:

--- challenge16.php
+++ challenge16.php
@@ -1,6 +1,6 @@
 <?php
     function highAndLow($numbers) {
-    $sortedArray = explode(" ", $numbers);
-    sort($sortedArray);
-    return $sortedArray[sizeof($sortedArray) - 1] . " " . $sortedArray[0];
+        $sortedArray = explode(" ", $numbers);
+        sort($sortedArray);
+        return $sortedArray[sizeof($sortedArray) - 1] . " " . $sortedArray[0];
     }
